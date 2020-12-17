96f8895fb55371e64a5abcb62db1f5410438b448
chenjingming <chenjingming@300.cn>
2020-12-17 14:54:17 +0800

    多环境构建脚本

--- deploy.js
+++ deploy.js
@@ -1,4 +1,5 @@
 const ci = require('miniprogram-ci')
+const pkg = require('./package.json')
 ;(async () => {
   const project = new ci.Project({
     appid: 'wxc326b824cc3d1077',
@@ -8,10 +9,10 @@ const ci = require('miniprogram-ci')
     ignores: ['node_modules/**/*'],
   })
   try {
-    const previewResult = await ci.preview({
+    await ci.preview({
       project,
       robot: 2,
-      desc: 'dev/V1.0.0', // 此备注将显示在“小程序助手”开发版列表中
+      desc: `${process.env.HOTEL_APP_ENV} / ${pkg.version}`, // 此备注将显示在“小程序助手”开发版列表中
       setting: {
         es6: true,
       },
@@ -21,7 +22,6 @@ const ci = require('miniprogram-ci')
       // pagePath: 'pages/index/index', // 预览页面
       // searchQuery: 'a=1&b=2',   // 预览参数 [注意!]这里的`&`字符在命令行中应写成转义字符`\&`
     })
-    console.log(previewResult)
   } catch (err) {
     console.log('err', err)
   }
